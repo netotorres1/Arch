715dcc5f2348e78d89114c87aa37a6ea2238dfa7
netotorres1 <torresneto47@gmail.com>
2022-12-16 04:13:13 -0300

    pag portifolio em andamento

--- src/Components/PortifolioComponent/PortifolioComponent.jsx
+++ src/Components/PortifolioComponent/PortifolioComponent.jsx
@@ -1,9 +1,90 @@
-import { Container } from './style'
+import { Container, SeraphStation, EeboxBuilding, FederalTower, ProjectDelSol, LePrototype, Tower, Row, ContainerContent, ContainerText, Title, LinkProjects, GrandEdelweissHotel, NectryTower, Hypers, TowerSXIV, TrinityBankTower, ProjectParamour } from './style'
 
 const PortifolioComponent = () => {
     return(
         <Container>
-            Teste
+            <ContainerContent>
+                <Row>
+                    <SeraphStation>
+                        <ContainerText>
+                            <Title>Seraph Station</Title>
+                            <LinkProjects>September 2019</LinkProjects>
+                        </ContainerText>
+                    </SeraphStation>
+                    <EeboxBuilding>
+                        <ContainerText>
+                            <Title>Eebox Building</Title>
+                            <LinkProjects>August 2017</LinkProjects>
+                        </ContainerText>
+                    </EeboxBuilding>
+                    <FederalTower>
+                        <ContainerText>
+                            <Title>Federal II Tower</Title>
+                            <LinkProjects>March 2017</LinkProjects>
+                        </ContainerText>
+                    </FederalTower>
+                </Row>
+                <Row>
+                    <ProjectDelSol>
+                        <ContainerText>
+                            <Title>Project Del Sol</Title>
+                            <LinkProjects>January 2016</LinkProjects>
+                        </ContainerText>
+                    </ProjectDelSol>
+                    <LePrototype>
+                        <ContainerText>
+                            <Title>Le Prototype</Title>
+                            <LinkProjects>October 2015</LinkProjects>
+                        </ContainerText>
+                    </LePrototype>
+                    <Tower>
+                        <ContainerText>
+                            <Title>228B Tower</Title>
+                            <LinkProjects>April 2015</LinkProjects>
+                        </ContainerText>
+                    </Tower>
+                </Row>
+                <Row>
+                    <GrandEdelweissHotel>
+                        <ContainerText>
+                            <Title>Grand Edelweiss Hotel</Title>
+                            <LinkProjects>December 2013</LinkProjects>
+                        </ContainerText>
+                    </GrandEdelweissHotel>
+                    <NectryTower>
+                        <ContainerText>
+                            <Title>Netcry Tower</Title>
+                            <LinkProjects>August 2012</LinkProjects>
+                        </ContainerText>
+                    </NectryTower>
+                    <Hypers>
+                        <ContainerText>
+                            <Title>Hypers</Title>
+                            <LinkProjects>January 2012</LinkProjects>
+                        </ContainerText>
+                    </Hypers>
+                </Row>
+                <Row>
+                    <TowerSXIV>
+                        <ContainerText>
+                            <Title>SXIV Tower</Title>
+                            <LinkProjects>March 2011</LinkProjects>
+                        </ContainerText>
+                    </TowerSXIV>
+                    <TrinityBankTower>
+                        <ContainerText>
+                            <Title>Trinity Bank Tower</Title>
+                            <LinkProjects>September 2017</LinkProjects>
+                        </ContainerText>
+                    </TrinityBankTower>
+                    <ProjectParamour>
+                        <ContainerText>
+                            <Title>Project Paramour</Title>
+                            <LinkProjects>Frebruary 2008</LinkProjects>
+                        </ContainerText>
+                    </ProjectParamour>
+                </Row>
+            </ContainerContent>
         </Container>
     )
 }
--- src/Components/PortifolioComponent/style.js
+++ src/Components/PortifolioComponent/style.js
@@ -1,5 +1,205 @@
 import styled from 'styled-components'
+import {mobile, tablet} from './../../responsive'
+
+import Image228b from './../../assets/portfolio/desktop/image-228b.jpg'
+import Image228bTablet from './../../assets/portfolio/tablet/image-228b.jpg'
+
+import DelSol from './../../assets/portfolio/desktop/image-del-sol.jpg'
+import DelSolTablet from './../../assets/portfolio/tablet/image-del-sol.jpg'
+
+import Edelweiss from './../../assets/portfolio/desktop/image-edelweiss.jpg'
+import EdelweissTablet from './../../assets/portfolio/tablet/image-edelweiss.jpg'
+
+import Eebox from './../../assets/portfolio/desktop/image-eebox.jpg'
+import EeboxTablet from './../../assets/portfolio/tablet/image-eebox.jpg'
+
+import federal from './../../assets/portfolio/desktop/image-federal.jpg'
+import federalTablet from './../../assets/portfolio/tablet/image-federal.jpg'
+
+import ImageHypers from './../../assets/portfolio/desktop/image-hypers.jpg'
+import ImageHypersTablet from './../../assets/portfolio/tablet/image-hypers.jpg'
+
+import netcry from './../../assets/portfolio/desktop/image-netcry.jpg'
+import netcryTablet from './../../assets/portfolio/tablet/image-netcry.jpg'
+
+import paramour from './../../assets/portfolio/desktop/image-paramour.jpg'
+import paramourTablet from './../../assets/portfolio/tablet/image-paramour.jpg'
+
+import prototype from './../../assets/portfolio/desktop/image-prototype.jpg'
+import prototypeTablet from './../../assets/portfolio/tablet/image-prototype.jpg'
+
+import Seraph from './../../assets/portfolio/desktop/image-seraph.jpg'
+import SeraphTablet from './../../assets/portfolio/tablet/image-seraph.jpg'
+
+
+import sxiv from './../../assets/portfolio/desktop/image-sxiv.jpg'
+import sxivTablet from './../../assets/portfolio/tablet/image-sxiv.jpg'
+
+import trinity from './../../assets/portfolio/desktop/image-trinity.jpg'
+import trinityTablet from './../../assets/portfolio/tablet/image-trinity.jpg'
 
 export const Container = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    ${tablet({width: '768px'})};
+`
+export const ContainerContent = styled.section`
+    width: 75%;
+    ${tablet({width: '90%'})};
+    
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 50px;
+    ${tablet({flexDirection: 'column'})};
+    ${tablet({width: '100%'})};
+    ${tablet({height: '1000px'})};
+`
+
+export const SeraphStation = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-color: red;
+    background-image: url(${Seraph});
+    background-size: cover;
+
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${SeraphTablet})`})};
+    
+`
+
+export const EeboxBuilding = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${Eebox});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${EeboxTablet})`})};
+`
+
+export const FederalTower = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${federal});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${federalTablet})`})};
+`
+
+export const ProjectDelSol = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${DelSol});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${DelSolTablet})`})};
+`
+
+export const LePrototype = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${prototype});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${prototypeTablet})`})};
+`
+
+export const Tower = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${Image228b});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${sxivTablet})`})};
+`
+
+export const GrandEdelweissHotel = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${Edelweiss});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${EdelweissTablet})`})};
+`
+
+export const NectryTower = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${netcry});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${netcryTablet})`})};
+`
+
+export const TowerSXIV = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${sxiv});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${sxivTablet})`})};
+`
+
+export const TrinityBankTower = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${trinity});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${trinityTablet})`})};
+`
+
+export const Hypers = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${ImageHypers});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${ImageHypersTablet})`})};
+`
+
+export const ProjectParamour = styled.div`
+    height: 500px; 
+    width: 30%;
+    background-image: url(${paramour});
+    background-size: cover;
+    ${tablet({width: '100%'})};
+    ${tablet({height: '300px'})};
+    ${tablet({backgroundImage: `url(${paramourTablet})`})};
+`
+
+
+
+export const ContainerText = styled.div`
+    position: relative;
+    top: 420px;
+    left: 40px;
+    color: white;
+    ${tablet({top: '220px'})};
+    font-family: 'League Spartan', sans-serif;
+    width: 300px;
+`
+
+export const Title = styled.h2`
+    text-shadow:2px 2px 5px #000000;
+`
+
+export const LinkProjects = styled.span`
+    text-shadow:2px 2px 5px #000000;
 
 `
